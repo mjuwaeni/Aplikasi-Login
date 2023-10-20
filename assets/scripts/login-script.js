6bc7ec3d602ef8f944f0188cc3fbcf13b20c65d2
mjuwaeni <50060758+mjuwaeni@users.noreply.github.com>
2023-10-20 21:22:18 +0700

    Update login-script.js
    mengganti komentar sesuai dengan JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,11 +1,13 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script*/
+/**
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
